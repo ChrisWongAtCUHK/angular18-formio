ba12614910328c14184dad9709d2f6ddbd3fc034
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-17 17:35:57 +0800

    Use Form.io

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { SimpleComponent } from './simple/simple.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [SimpleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
